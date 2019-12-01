303096653edd7ca44418582ad084bdfb53ddd1aa
xj_j <1447177020@qq.com>
2019-12-01 18:41:49 +0800

    #SpringSecurity加密utils

---
+++ permissionManager_ssm_utils/src/main/java/cn/blithe/ssm/utils/BCryptPasswordEncoderUtils.java
@@ -0,0 +1,24 @@
+package cn.blithe.ssm.utils;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+/**
+ * @ClassName BCryptPasswordEncoder
+ * @Deacription SpringSecurity加密
+ * @Author 夏小颜
+ * @Date 2019/12/1 18:37
+ * @Version 1.0
+ **/
+public class BCryptPasswordEncoderUtils {
+
+    private static BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+
+    public static String BCryptPasswordEncoder(String password){
+        return bCryptPasswordEncoder.encode(password);
+    }
+
+    public static void main(String[] args) {
+        String pwd = BCryptPasswordEncoder("123");
+        System.out.println(pwd);
+    }
+}
